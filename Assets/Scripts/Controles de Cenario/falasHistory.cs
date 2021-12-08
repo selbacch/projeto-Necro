099ee719e190d52a3085c551e9d931e023a8b90f
selbacch <75805451+selbacch@users.noreply.github.com>
2021-12-07 22:37:20 -0300

    texto que conta a historia da espada

---
+++ Assets/Scripts/Controles de Cenario/falasHistory.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class falasHistory : MonoBehaviour
+{
+
+    public string[] txt;
+    private int Dialogo = 0;
+    public TMP_Text texto;
+    public float Time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    IEnumerator falas()
+    {
+        for (Dialogo = 0; Dialogo < txt.Length; Dialogo++)
+        {
+            texto.text = txt[Dialogo];
+            yield return new WaitForSecondsRealtime(Time);
+        }
+    }
+
+
+
+
+}
