ad6f7615eda6c410dd775af2fa0a0e8ec26af070
selbacch <75805451+selbacch@users.noreply.github.com>
2021-12-11 22:14:12 -0300

    boss musica

---
+++ Assets/Scripts/Controles de Cenario/Relampago.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+public class Relampago : MonoBehaviour
+{
+
+    public Light2D Light;
+    public Light2D Light2;
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Light.intensity = 145f;
+        Light.intensity = 145f;
+
+
+
+
+
+    }
+}
